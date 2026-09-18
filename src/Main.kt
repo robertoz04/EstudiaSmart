898e8b7f1b5995d805733ed1cc636eb84503f69d
Roberto Zavaleta <roerickzavaleta@gmail.com>
2026-09-17 22:40:26 -0600

    feat: implementar calculo de dominio por tema

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,5 @@
-import model.Pregunta
-import model.Quiz
-import service.QuizService
+import model.ProgresoTema
+import service.ProgresoService
 
 fun main() {
 
@@ -8,57 +7,27 @@ fun main() {
     println("          ESTUDIASMART")
     println("================================")
 
-    val pregunta1 = Pregunta(
-        id = 1,
-        quizId = 1,
-        enunciado = "¿Qué concepto permite que una clase herede de otra?",
-        opcionA = "Encapsulamiento",
-        opcionB = "Herencia",
-        opcionC = "Abstracción",
-        opcionD = "Sobrecarga",
-        respuestaCorrecta = 'B'
+    val progreso = ProgresoTema(
+        temaId = 1
     )
 
-    val pregunta2 = Pregunta(
-        id = 2,
-        quizId = 1,
-        enunciado = "¿Cuál palabra se utiliza para declarar una clase en Kotlin?",
-        opcionA = "class",
-        opcionB = "object",
-        opcionC = "struct",
-        opcionD = "model",
-        respuestaCorrecta = 'A'
+    val progresoService = ProgresoService()
+
+    val precisionQuiz = 80.0
+    val rendimientoFlashcards = 90.0
+
+    progresoService.actualizarProgreso(
+        progreso,
+        precisionQuiz,
+        rendimientoFlashcards
     )
 
-    val quiz = Quiz(
-        id = 1,
-        temaId = 1,
-        nombre = "Quiz de Programación Orientada a Objetos"
-    )
-
-    quiz.preguntas.add(pregunta1)
-    quiz.preguntas.add(pregunta2)
-
-    val respuestas = mapOf(
-        1 to 'B',
-        2 to 'C'
-    )
-
-    val quizService = QuizService()
-
-    val resultado = quizService.evaluarQuiz(
-        quiz = quiz,
-        respuestasUsuario = respuestas,
-        idIntento = 1
-    )
-
-    println("\nResultado del Quiz")
+    println("\nPROGRESO DEL TEMA")
     println("--------------------------------")
-    println("Correctas: ${resultado.respuestasCorrectas}")
-    println("Total: ${resultado.totalPreguntas}")
-    println("Puntuación: ${resultado.puntuacion}%")
+    println("Rendimiento Quiz: ${progreso.precisionQuiz}%")
+    println("Rendimiento Flashcards: ${progreso.rendimientoFlashcards}%")
+    println("Dominio total: ${progreso.dominio}%")
     println(
-        "Nivel: ${quizService.obtenerNivelDominio(resultado.puntuacion)}"
+        "Nivel: ${progresoService.obtenerNivelDominio(progreso.dominio)}"
     )
-    println("Preguntas falladas: ${resultado.preguntasFalladas}")
 }
