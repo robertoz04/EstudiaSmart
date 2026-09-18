1fb7076fdc5d79b40623f69359f956208562973f
Roberto Zavaleta <roerickzavaleta@gmail.com>
2026-09-17 22:29:33 -0600

    feat: implementar repeticion espaciada de flashcards

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,37 @@
+import model.Dificultad
+import model.Flashcard
+import service.FlashcardService
+
 fun main() {
+
     println("================================")
     println("          ESTUDIASMART")
     println("================================")
-    println("Bienvenido a EstudiaSmart")
+
+    val flashcard = Flashcard(
+        id = 1,
+        temaId = 1,
+        pregunta = "¿Qué es la herencia?",
+        respuesta = "Es un mecanismo de POO que permite heredar características de otra clase."
+    )
+
+    val flashcardService = FlashcardService()
+
+    println("\nFlashcard:")
+    println(flashcard.pregunta)
+
+    println("\nRespuesta:")
+    println(flashcard.respuesta)
+
+    flashcardService.revisarFlashcard(
+        flashcard,
+        Dificultad.REGULAR,
+        true
+    )
+
+    println("\nResultado de la revisión")
+    println("Dificultad: ${flashcard.dificultad}")
+    println("Racha de aciertos: ${flashcard.rachaAciertos}")
+    println("Última revisión: ${flashcard.ultimaRevision}")
+    println("Próxima revisión: ${flashcard.proximaRevision}")
 }
