44b9f83977915bb739403b465f45ade8e7f6bde2
Roberto Zavaleta <roerickzavaleta@gmail.com>
2026-09-17 22:02:54 -0600

    chore: crear estructura base del proyecto Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("================================")
+    println("          ESTUDIASMART")
+    println("================================")
+    println("Bienvenido a EstudiaSmart")
+}
